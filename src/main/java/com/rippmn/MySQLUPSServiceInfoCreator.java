69b841d1a05eb6a79ba0bd7209ea1a1f9d463455
Andrew Ripka <aripka@pivotal.io>
2014-12-05 09:31:11 -0600

    change class heirarchy to leverage code that is same as parent

--- src/main/java/com/rippmn/MySQLUPSServiceInfoCreator.java
+++ src/main/java/com/rippmn/MySQLUPSServiceInfoCreator.java
@@ -2,16 +2,9 @@ package com.rippmn;
 
 import java.util.Map;
 
-import org.springframework.cloud.cloudfoundry.RelationalServiceInfoCreator;
-import org.springframework.cloud.cloudfoundry.Tags;
-import org.springframework.cloud.service.common.MysqlServiceInfo;
+import org.springframework.cloud.cloudfoundry.MysqlServiceInfoCreator;
 
-public class MySQLUPSServiceInfoCreator extends RelationalServiceInfoCreator<MysqlServiceInfo>{
-	
-	
-	public MySQLUPSServiceInfoCreator() {
-		super(new Tags(), "mysql");
-	}
+public class MySQLUPSServiceInfoCreator extends MysqlServiceInfoCreator{
 	
 	
 	@Override
@@ -24,12 +17,5 @@ public class MySQLUPSServiceInfoCreator extends RelationalServiceInfoCreator<Mys
 			accept = serviceData.get("label")!= null && serviceData.get("label").equals("user-provided") && credentials.get("ups-type") != null && credentials.get("ups-type").equals("mysql-db"); 
 		return accept || super.accept(serviceData);		
 	}
-
-
-	@Override
-	public MysqlServiceInfo createServiceInfo(String id, String uri) {
-		return new MysqlServiceInfo(id, uri);
-	}
-	
 
 }
